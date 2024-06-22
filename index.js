bc5f863a7a5735a2fc30b56e1b928cb9666fcd86
renan <renanbazinin2@gmail.com>
2024-06-22 14:52:22 +0300

    Added

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ const express = require('express');
 const { fetchBBCNewsRSS, fetchNYTNewsRSS } = require('./fetchNews');
 const path = require('path');
 const cors = require('cors');
-const dotenv = require('dotenv');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Use CORS middleware
 app.use(cors());
-dotenv.config();
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
